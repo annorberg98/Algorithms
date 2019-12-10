35fbd18595b23b03b877308216a127220ca6a0fb
AntonNorberg <antonnorberg98@gmail.com>
2019-12-10 14:05:33 +0100

    Finished quicksort

--- Program.cs
+++ Program.cs
@@ -4,46 +4,66 @@ namespace Programmeringuppgift
 {
     class Program
     {
-
-        private void RunQuickSort(string filename)
+        static int Partition(int[] array, int low, int high)
         {
-            int[] arr = MyInsertionTest.ReadIntfile(filename);
-            int N = arr.Length;
+            //1. Select a pivot point.
+            int pivot = array[high];
 
-            if (N <= 1000)
+            int lowIndex = (low - 1);
+
+            //2. Reorder the collection.
+            for (int j = low; j < high; j++)
             {
-                for (int i = 0; i < N; i++)
+                if (array[j] <= pivot)
                 {
-                    Console.Write(arr[i] + " ");
+                    lowIndex++;
+
+                    int temp = array[lowIndex];
+                    array[lowIndex] = array[j];
+                    array[j] = temp;
                 }
-                Console.WriteLine("\n\n");
             }
+
+            int temp1 = array[lowIndex + 1];
+            array[lowIndex + 1] = array[high];
+            array[high] = temp1;
+
+            return lowIndex + 1;
+        }
+
+        static void Sort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int partitionIndex = Partition(array, low, high);
+
+                //3. Recursively continue sorting the array
+                Sort(array, low, partitionIndex - 1);
+                Sort(array, partitionIndex + 1, high);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            int[] data = MyInsertionTest.ReadIntfile("largeints");
+            int N = data.Length;
+
+            Console.WriteLine("QuickSort");
 
             long before = Environment.TickCount;
-            Quicksort.Sort(arr, 0, arr.Length-1);
+            Sort(data, 0, N - 1);
             long after = Environment.TickCount;
 
-            // Look at numbers after sorting, unless there are too many of them.
-            if (N <= 1000)
-            {
-                for (int i = 0; i < N; i++)
-                {
-                    Console.Write(arr[i] + " ");
-                }
-                Console.Write("\n");
-            }
-
-            if (MyInsertionTest.IsSorted(arr, 0, N - 1))
+            if (MyInsertionTest.IsSorted(data, 0, N - 1))
             {
                 Console.WriteLine((after - before) / 1000.0 + " seconds");
             }
 
-            Console.ReadLine();
+            Console.ReadKey();
         }
 
-        private void RunMergesort(string filename)
+        /*static void Main(string[] args)
         {
-            int[] data = MyInsertionTest.ReadIntfile(filename);
+            int[] data = MyInsertionTest.ReadIntfile("largeints");
             int N = data.Length;
 
             if (N <= 1000)
@@ -75,22 +95,6 @@ namespace Programmeringuppgift
             }
 
             Console.ReadLine();
-        }
-
-        public void Run(string type,string filename)
-        {
-            if(type == "quick")
-            {
-                this.RunQuickSort(filename);
-            } else if(type == "merge")
-            {
-                this.RunMergesort(filename);
-            }
-        }
-        
-        static void Main(string[] args)
-        {
-            
-        }
+        }*/
     }
 }
--- Quicksort.cs
+++ Quicksort.cs
@@ -6,51 +6,69 @@ namespace Programmeringuppgift
 {
     class Quicksort
     {
-        public static int Partition(int[] arr, int left, int right)
+        public int[] array;
+        private int N;
+
+        public Quicksort(int[] array)
         {
-            int pivot;
-            pivot = arr[left];
-            while (true)
+            this.array = array;
+            this.N = this.array.Length;
+        }
+
+        public int[] Sort()
+        {
+            return sort(0, N - 1);
+        }
+
+        private int[] sort(int left, int right)
+        {
+            int pivot, leftend, rightend;
+
+            leftend = left;
+            rightend = right;
+            pivot = array[left];
+
+            while (left < right)
             {
-                while (arr[left] < pivot)
-                {
-                    left++;
-                }
-                while (arr[right] > pivot)
+                while ((array[right] >= pivot) && (left < right))
                 {
                     right--;
                 }
-                if (left < right)
+
+                if (left != right)
                 {
-                    int temp = arr[right];
-                    arr[right] = arr[left];
-                    arr[left] = temp;
+                    array[left] = array[right];
+                    left++;
                 }
-                else
+
+                while ((array[left] <= pivot) && (left < right))
                 {
-                    return right;
+                    left++;
+                }
+
+                if (left != right)
+                {
+                    array[right] = array[left];
+                    right--;
                 }
             }
 
-        }
+            array[left] = pivot;
+            pivot = left;
+            left = leftend;
+            right = rightend;
 
-        public static void Sort(int[] arr, int left, int right)
-        {
-            int pivot;
-            if (left < right)
+            if (left < pivot)
             {
-                pivot = Partition(arr, left, right);
-                if (pivot > 1)
-                {
-                    Sort(arr, left, pivot - 1);
-                }
-                if (pivot + 1 < right)
-                {
-                    Sort(arr, pivot + 1, right);
-                }
+                sort(left, pivot - 1);
             }
+
+            if (right > pivot)
+            {
+                sort(pivot + 1, right);
+            }
+
+            return this.array;
         }
-
-
     }
 }
